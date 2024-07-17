6a1174f647502f8fd9c9cc16f1124c5ef8133126
AdwaithVS <adwaithvs512@gmail.com>
2024-07-17 19:59:32 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AddPage/>}/>
-        <Route path="/search" element={<SearchPage/>}/>
-        <Route path="/view" element={<ViewPage/>}/>
-      </Routes>
+    <Routes>
+      <Route path="/" element={<AddPage/>}/>
+      <Route path="/search" element={<SearchPage/>}/>
+      <Route path="/view" element={<ViewPage/>}/>
+    </Routes>
     </BrowserRouter>
   );
 }

f94d3de5261e26c750206cd3b151d3734d4207fb
AdwaithVS <adwaithvs512@gmail.com>
2024-07-17 19:55:00 +0530

    Almost Completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import AddPage from './components/AddPage';
+import SearchPage from './components/SearchPage';
 import ViewPage from './components/ViewPage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <ViewPage/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AddPage/>}/>
+        <Route path="/search" element={<SearchPage/>}/>
+        <Route path="/view" element={<ViewPage/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
